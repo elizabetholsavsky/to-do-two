6389a3925a839105da704b4c26966c57fa2ef6f8
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2024-01-17 14:11:42 -0600

    add falling transition on delete btn

--- app.js
+++ app.js
@@ -40,7 +40,11 @@ function deleteCheck(e) {
     //Delete todo
     if(item.classList[0] === 'trash-btn') {
         const todo = item.parentElement;
-        todo.remove();
+        //animation
+        todo.classList.add("fall");
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
     }
 
     if(item.classList[0] === 'complete-btn') {
